667f8efcdca4faae752e994d8f91607b209be3a5
Aayushi Agrawal <aayushi.a@go-jek.com>
2019-12-12 12:41:53 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,64 +1,70 @@
-import React from "react";
-import ReactDOMServer from "react-dom/server";
-
-import PillView from "./pill-view";
-
 const pill = {
-  label: "Pill",
-  name: "pill",
+  // Internal id of the component
   id: "pill",
-  fromBlock: match =>
-    match && {
-      pill: match[2]
-    },
-  //to preview the editor_preview(right side)
-  toBlock: ({ pill, textColor, bgColor, borderColor }) => {
-    return ReactDOMServer.renderToStaticMarkup(
-      <PillView
-        pill={pill}
-        textColor={textColor}
-        bgColor={bgColor}
-        borderColor={borderColor}
-      />
-    );
-  },
-  // to preview output in the website
-  toPreview: ({ pill, textColor, bgColor, borderColor }) => {
-    return (
-      <PillView
-        pill={pill}
-        textColor={textColor}
-        bgColor={bgColor}
-        borderColor={borderColor}
-      />
-    );
-  },
+  // Visible label
+  label: "Pill",
+  // Fields the user need to fill out when adding an instance of the component
   fields: [
     {
-      label: "Status",
-      name: "pill",
-      widget: "string"
-    },
-    {
-      label: "Background color",
-      name: "bgColor",
+      name: "text",
+      label: "Text",
       widget: "string",
-      default: `hsla(293, 26%, 93%, 1)`
     },
     {
-      label: "Text color",
+      name: "bgColor",
+      label: "Background Color",
+      widget: "string",
+      default: `#F1E9F2`,
+    },
+    {
+      label: "Text Color",
       name: "textColor",
       widget: "string",
-      default: `black`
+      default: `black`,
     },
     {
-      label: "Border color",
+      label: "Border Color",
       name: "borderColor",
       widget: "string",
-      default: `hsla(293, 32%, 46%, 1)`
-    }
+      default: `#92509B`,
+    },
   ],
-  pattern: /^<div id="cmsEdtiorComponentPill">(.*)<\/div>/
+  // Pattern to identify a block as being an instance of this component
+  // Function to extract data elements from the regexp match
+
+  pattern: /^{{< Pill text="(.*)" bgColor="(.*)" textColor="(.*)" borderColor="(.*)" >}}$/,
+  // The pattern is used to determine which parts of your content should be represented by the editor component, toBlock converts the matched content from raw Markdown/HTML to a data object,
+  fromBlock: match =>
+    match && {
+      text: match[1],
+      bgColor: match[2],
+      textColor: match[3],
+      borderColor: match[4],
+    },
+
+  toBlock: function(obj) {
+    return `{{< Pill text=\"${obj.text}\" bgColor=\"${obj.bgColor}\" textColor=\"${obj.textColor}\" borderColor=\"${obj.borderColor}\" >}}`
+  },
+  // Preview output for this component. Can either be a string or a React component (component gives better render performance)
+  toPreview: function(obj) {
+    return `<div
+      style=" 
+        background-color:${obj.bgColor};
+        border:1px solid ${obj.borderColor};
+        color:${obj.textColor};
+        text-align:center;
+        font-size:0.75em;
+        display:inline-block;
+        vertical-align:middle;
+        font-weight:500;  
+        border-radius:10rem;
+        padding:2px 10px;
+        margin-right:10px;
+      "
+    >
+      ${obj.text}
+    </div>`
+  },
 };
 
 export const NetlifyCmsEditorComponentPill = pill;
